55b984f8519834dd78bba321a6a63b25dd65d9a0
Eslam Akram <eslam_akram91@yahoo.com>
2021-08-01 16:29:18 +0300

    created objects

---
+++ js/app.js
@@ -0,0 +1,32 @@
+
+//object for each Location
+
+const Seattle ={
+    MinPerCust : 23,
+    MaxPerCust : 65,
+    AvgCookiePerSale : 6.3
+}
+
+const Tokyo ={
+    MinPerCust : 3,
+    MaxPerCust : 24,
+    AvgCookiePerSale : 1.2
+}
+
+const Dubai ={
+    MinPerCust : 11,
+    MaxPerCust : 38,
+    AvgCookiePerSale : 3.7
+}
+
+const Paris ={
+    MinPerCust : 20,
+    MaxPerCust : 38,
+    AvgCookiePerSale : 2.3
+}
+
+const Lima ={
+    MinPerCust : 2,
+    MaxPerCust : 16,
+    AvgCookiePerSale : 4.6
+}
