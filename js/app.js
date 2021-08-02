0ddec3ef6c41d70f30fab6bf0db8bc07fdeb3421
Eslam Akram <eslam_akram91@yahoo.com>
2021-08-02 17:09:08 +0300

    part1 single constructor had been solved

--- js/app.js
+++ js/app.js
@@ -1,210 +1,32 @@
+// single constructor
+   function locationSales( locationName, MinPerCust, MaxPerCust, AvgCookiePerSale ){
+this.locationName = locationName;
+this.MinPerCust = MinPerCust;
+this.MaxPerCust = MaxPerCust;
+this.AvgCookiePerSale = AvgCookiePerSale;
+//this.NoOfCustomersPerHour = NoOfCustomersPerHour;
+this.hours = ['6:00 AM: ','7:00 AM: ','8:00 AM: ','9:00 AM: ','10:00 AM: ','11:00 AM: ','12:00 PM: ','1:00 PM: ','2:00 PM: ','3:00 PM: ','4:00 PM: ','5:00 PM: ','6:00 PM: ','7:00 PM: '];
+//this.AvgCookiesPerHour = AvgCookiesPerHour;
+this.Total= 0;
+this.NoOfCustomersPerHour = new Array(13);
+this.AvgCookiesPerHour = new Array(13);
+
+} //end of const
 
 
-
-
-
-//object for each Location
-
-const Seattle ={
-    locationName : 'Seattle Location',
-    MinPerCust : 23,
-    MaxPerCust : 65,
-    AvgCookiePerSale : 6.3,
-    NoOfCustomersPerHour : new Array(13) ,
-    hours : ['6 AM: ','7 AM: ','8 AM: ','9 AM: ','10 AM: ','11 AM: ','12 PM: ','1 PM: ','2 PM: ','3 PM: ','4 PM: ','5 PM: ','6 PM: ','7 PM: '],
-    AvgCookiesPerHour : new Array(13),
-    Total : 0,
-   getNoOfCustomer: function() {
-       let FinalResult = 0;
-        for (let i=0 ; i < this.hours.length ; i++){
-        this.NoOfCustomersPerHour [i] = Math.floor(Math.random() * (this.MaxPerCust - this.MinPerCust + 1) + this.MinPerCust);
-        this.AvgCookiesPerHour [i] = Math.floor(this.NoOfCustomersPerHour[i] * this.AvgCookiePerSale);
-        FinalResult =  FinalResult + this.AvgCookiesPerHour [i];
-                }
-                this.Total = FinalResult;    
-    },          
-   render: function () {
-
-    let seattleSale = document.getElementById('locationSales');
-
-    let articleElement = document.createElement('article');
-    seattleSale.appendChild(articleElement);
-    
-    
-    let h2Element = document.createElement('h2');
-    h2Element.textContent= this.locationName;
-    articleElement.appendChild(h2Element);
-
-    let ulElement = document.createElement('ul');
-    articleElement.appendChild(ulElement);
-
-    for (let x=0; x < 14; x++){
-        let liElement = document.createElement('li');
-        liElement.textContent = this.hours[x] + this.AvgCookiesPerHour[x]  + ' Cookies';
-        ulElement.appendChild(liElement);
-       // console.log(liElement);
-           }
-    
-    let pElement = document.createElement('p');
-    pElement.textContent = 'The Total :' + this.Total;
-    articleElement.appendChild(pElement);
-        }
-};
-
-const Tokyo ={
-    locationName : 'Tokyo Location',
-    MinPerCust : 3,
-    MaxPerCust : 24,
-    AvgCookiePerSale : 1.2,
-    NoOfCustomersPerHour : new Array(13) ,
-    hours : ['6 AM: ','7 AM: ','8 AM: ','9 AM: ','10 AM: ','11 AM: ','12 PM: ','1 PM: ','2 PM: ','3 PM: ','4 PM: ','5 PM: ','6 PM: ','7 PM: '],
-    AvgCookiesPerHour : new Array(13),
-    Total : 0,
-   getNoOfCustomer: function() {
-       let FinalResult = 0;
-        for (let i=0 ; i < this.hours.length ; i++){
-        this.NoOfCustomersPerHour [i] = Math.floor(Math.random() * (this.MaxPerCust - this.MinPerCust + 1) + this.MinPerCust);
-        this.AvgCookiesPerHour [i] = Math.floor(this.NoOfCustomersPerHour[i] * this.AvgCookiePerSale);
-        FinalResult =  FinalResult + this.AvgCookiesPerHour [i];
-                }
-                this.Total = FinalResult;    
-    },          
-   render: function () {
-
-    let tokyoSale = document.getElementById('locationSales');
-
-    let articleElement = document.createElement('article');
-    tokyoSale.appendChild(articleElement);
-    
-    
-    let h2Element = document.createElement('h2');
-    h2Element.textContent= this.locationName;
-    articleElement.appendChild(h2Element);
-
-    let ulElement = document.createElement('ul');
-    articleElement.appendChild(ulElement);
-
-    for (let x=0; x < 14; x++){
-        let liElement = document.createElement('li');
-        liElement.textContent = this.hours[x] + this.AvgCookiesPerHour[x]  + ' Cookies';
-        ulElement.appendChild(liElement);
-        console.log(liElement);
-           }
-    
-    let pElement = document.createElement('p');
-    pElement.textContent = 'The Total :' + this.Total;
-    articleElement.appendChild(pElement);
-        }
-    
-};
-
-const Dubai ={
-    locationName : 'Dubai Location',
-    MinPerCust : 11,
-    MaxPerCust : 38,
-    AvgCookiePerSale : 3.7 ,
-    NoOfCustomersPerHour : new Array(13) ,
-    hours : ['6 AM: ','7 AM: ','8 AM: ','9 AM: ','10 AM: ','11 AM: ','12 PM: ','1 PM: ','2 PM: ','3 PM: ','4 PM: ','5 PM: ','6 PM: ','7 PM: '],
-    AvgCookiesPerHour : new Array(13),
-    Total : 0,
-   getNoOfCustomer: function() {
-       let FinalResult = 0;
-        for (let i=0 ; i < this.hours.length ; i++){
-        this.NoOfCustomersPerHour [i] = Math.floor(Math.random() * (this.MaxPerCust - this.MinPerCust + 1) + this.MinPerCust);
-        this.AvgCookiesPerHour [i] = Math.floor(this.NoOfCustomersPerHour[i] * this.AvgCookiePerSale);
-        FinalResult =  FinalResult + this.AvgCookiesPerHour [i];
-                }
-                this.Total = FinalResult;    
-    },          
-   render: function () {
-
-    let dubaiSale = document.getElementById('locationSales');
-
-    let articleElement = document.createElement('article');
-    dubaiSale.appendChild(articleElement);
-    
-    
-    let h2Element = document.createElement('h2');
-    h2Element.textContent= this.locationName;
-    articleElement.appendChild(h2Element);
-
-    let ulElement = document.createElement('ul');
-    articleElement.appendChild(ulElement);
-
-    for (let x=0; x < 14; x++){
-        let liElement = document.createElement('li');
-        liElement.textContent= this.hours[x] + this.AvgCookiesPerHour[x] + ' Cookies';
-        ulElement.appendChild(liElement);
+locationSales.prototype.getNoOfCustomer = function(){
+    let FinalResult = 0;
+       for (let i=0 ; i < this.hours.length ; i++){
+    this.NoOfCustomersPerHour [i] = Math.floor(Math.random() * (this.MaxPerCust - this.MinPerCust + 1) + this.MinPerCust);
+    this.AvgCookiesPerHour [i] = Math.floor( this.NoOfCustomersPerHour[i] * this.AvgCookiePerSale);
+    FinalResult =  FinalResult + this.AvgCookiesPerHour [i];
             }
-    let pElement = document.createElement('p');
-    pElement.textContent = 'The Total :' + this.Total;
-    articleElement.appendChild(pElement);
-}
-};
+            this.Total = FinalResult; 
 
-const Paris ={
-    locationName : 'Paris Location',
-    MinPerCust : 20,
-    MaxPerCust : 38,
-    AvgCookiePerSale : 2.3 ,
-    NoOfCustomersPerHour : new Array(13) ,
-    hours : ['6 AM: ','7 AM: ','8 AM: ','9 AM: ','10 AM: ','11 AM: ','12 PM: ','1 PM: ','2 PM: ','3 PM: ','4 PM: ','5 PM: ','6 PM: ','7 PM: '],
-    AvgCookiesPerHour : new Array(13),
-    Total : 0,
-   getNoOfCustomer: function() {
-       let FinalResult = 0;
-        for (let i=0 ; i < this.hours.length ; i++){
-        this.NoOfCustomersPerHour [i] = Math.floor(Math.random() * (this.MaxPerCust - this.MinPerCust + 1) + this.MinPerCust);
-        this.AvgCookiesPerHour [i] = Math.floor(this.NoOfCustomersPerHour[i] * this.AvgCookiePerSale);
-        FinalResult =  FinalResult + this.AvgCookiesPerHour [i];
-                }
-                this.Total = FinalResult;    
-    },          
-   render: function () {
+} // end of prototype of getNoofCustomer()
 
-    let parisSale = document.getElementById('locationSales');
 
-    let articleElement = document.createElement('article');
-    parisSale.appendChild(articleElement);
-    
-    
-    let h2Element = document.createElement('h2');
-    h2Element.textContent= this.locationName;
-    articleElement.appendChild(h2Element);
-
-    let ulElement = document.createElement('ul');
-    articleElement.appendChild(ulElement);
-
-    for (let x=0; x < 14; x++){
-        let liElement = document.createElement('li');
-        liElement.textContent= this.hours[x] + this.AvgCookiesPerHour[x] + ' Cookies';
-        ulElement.appendChild(liElement);
-            }
-    let pElement = document.createElement('p');
-    pElement.textContent = 'The Total :' + this.Total;
-    articleElement.appendChild(pElement);
-}
-};
-
-const Lima = {
-    locationName : 'Lima Location',
-    MinPerCust : 2,
-    MaxPerCust : 16,
-    AvgCookiePerSale : 4.6,
-    NoOfCustomersPerHour : new Array(13) ,
-    hours : ['6 AM: ','7 AM: ','8 AM: ','9 AM: ','10 AM: ','11 AM: ','12 PM: ','1 PM: ','2 PM: ','3 PM: ','4 PM: ','5 PM: ','6 PM: ','7 PM: '],
-    AvgCookiesPerHour : new Array(13),
-    Total : 0,
-   getNoOfCustomer: function() {
-       let FinalResult = 0;
-        for (let i=0 ; i < this.hours.length ; i++){
-        this.NoOfCustomersPerHour [i] = Math.floor(Math.random() * (this.MaxPerCust - this.MinPerCust + 1) + this.MinPerCust);
-        this.AvgCookiesPerHour [i] = Math.floor(this.NoOfCustomersPerHour[i] * this.AvgCookiePerSale);
-        FinalResult =  FinalResult + this.AvgCookiesPerHour [i];
-                }
-                this.Total = FinalResult;    
-    },          
-   render: function () {
+locationSales.prototype.render = function(){
 
     let limaSale = document.getElementById('locationSales');
 
@@ -230,36 +52,28 @@ const Lima = {
     pElement.textContent = 'The Total :' + this.Total;
     articleElement.appendChild(pElement);
 
-//console.log(h2Element) ;
-//console.log(pElement);
-//console.log(liElement);
+} // end of prototype of render
 
+
+// defined the const proporties
+
+//object for each Location 
+
+let Seattle = new locationSales('Seattle Location', 23, 65, 6.3);
+let Tokyo = new locationSales('Tokyo Location', 3, 24, 1.2);
+let Dubai = new locationSales('Dubai Location', 11, 38, 3.7);
+let Paris = new locationSales('Paris Location', 20, 38, 2.3);
+let Lima = new locationSales('Lima Location', 2, 16, 4.6);
+ 
+// invoke it 
+
+let arr = [Seattle, Tokyo, Dubai, Paris, Lima];
+
+for(let i = 0; i < arr.length; i++) {
+  arr[i].getNoOfCustomer();
+  arr[i].render();
+  console.log(arr[i]);
 }
 
-};
-
-// Seattle loc
-Seattle.getNoOfCustomer();
-Seattle.render();
 
 
-
-// TOKYO LOC
-Tokyo.getNoOfCustomer();
-Tokyo.render();
-
-
-// DUBAI LOC
-//console.log(Dubai);
-Dubai.getNoOfCustomer();
-Dubai.render();
-
-// PARIS LOC
-console.log(Paris);
-Paris.getNoOfCustomer();
-Paris.render();
-
-// LIMA LOC
-console.log(Lima);
-Lima.getNoOfCustomer();
-Lima.render(); 
